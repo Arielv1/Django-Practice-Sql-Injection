d835a3c260fcb521280a87d69940f5d9613b7133
Arielv1 <arielv1994@gmail.com>
2020-10-25 21:16:01 +0200

    Added problems/2

--- ProjectCode/sql_training/problems/models.py
+++ ProjectCode/sql_training/problems/models.py
@@ -17,8 +17,13 @@ class FirstProblem(models.Model):
 
 
 class SecondProblem(models.Model):
-    name = models.TextField(max_length=200, primary_key=True, unique=True)
-    description = models.CharField(max_length=200, null=True)
+    id = models.BigIntegerField(primary_key=True, unique=True, default=0, null=False)
+    first_name = models.TextField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    age = models.IntegerField(null=True)
 
     def __str__(self):
-        return 'input1: {} input2: {} '.format(self.name, self.description)
+        return f'{self.id, self.first_name, self.last_name, self.age}'
+
+    #class Meta:
+    #    db_table = 'HR'
--- ProjectCode/sql_training/problems/views.py
+++ ProjectCode/sql_training/problems/views.py
@@ -7,7 +7,7 @@ from django.db import connections
 
 # Create your views here.
 from .models import CheckProblems
-from .models import FirstProblem
+from .models import *
 from collections import namedtuple
 
 
@@ -40,15 +40,27 @@ def first_problem(request):
     logger.error(" problem_login view called ")
     cursor1 = connections['problems_db'].cursor()
 
+    #table_name = FirstProblem.input1
+    #sql = "DROP TABLE problems_firstproblem;"
+    #cursor1.execute(sql)
+
     # make data permanent in the database
-    # for data in alldata:
-    #     data.save(using="problems_db")
+    #for data in alldata:
+    #    data.save(using="problems_db")
+
+    #for data in alldata:
+    #    data.clean()
+
     if request.method == 'POST':
-        logger.error(" request is Post ")
-        input1_request = request.POST.get("input1")
+
+
+
+
+       # logger.error(" request is Post ")
+       # input1_request = request.POST.get("input1")
         # input2_request = request.POST.get("input2")
         # logger.error("input1 is: " + input1_request + " input2 is: " + input2_request)
-        logger.error("input1 is: " + input1_request)
+      #  logger.error("input1 is: " + input1_request)
         # query = """ select input1, input2 from problems_firstproblem where input1 = 'input2_request' input1limit 1 """
         # query = 'select input1, input2 from problems_firstproblem limit 1'
         # query = "select input1, input2 from problems_firstproblem where input1='%s' limit 1"
@@ -59,15 +71,17 @@ def first_problem(request):
         # for result in results:
         #     logger.error(result)
 
-        # result = FirstProblem.objects.raw(using="problems_db"'SELECT * FROM problems_firstproblem')
+      #  # result = FirstProblem.objects.raw(using="problems_db"'SELECT * FROM problems_firstproblem')
         with cursor1 as cursor:
+
             # cursor.execute("select input1, input2 from problems_firstproblem where input1='%s' limit 1" % input1_request)
             # result = cursor.execute("select input1, input2 from problems_firstproblem where input2='%s'" % input1_request)
 
             # cursor.execute('SELECT * FROM problems_firstproblem WHERE input2=%s' % input1_request)
             # sql = f"SELECT * FROM problems_firstproblem WHERE input2 LIKE '%{input1_request}%';"
-            sql = f"SELECT * FROM problems_firstproblem WHERE input2 LIKE '{input1_request}';"
-
+            #sql = f"SELECT * FROM problems_firstproblem WHERE input2 LIKE '{input1_request}';"
+            #sql = f"SELECT * FROM problems_firstproblem WHERE input1 like '{input1_request}' "
+            sql = f"SELECT * FROM problems_firstproblem "
             cursor.execute(sql)
 
             # cursor.execute('SELECT * FROM problems_firstproblem WHERE input2=?' % input1_request)
@@ -75,7 +89,7 @@ def first_problem(request):
             # cursor.execute("select input1, input2 from problems_firstproblem where input2=%s", [input1_request])
             result = cursor.fetchall()
             # result = namedtuplefetchall(cursor)
-            logger.error(result)
+            logger.error("result of sql querry " + result.__str__())
             cursor.close()
             # allProblems = []
             # for data in result:
@@ -90,6 +104,7 @@ def first_problem(request):
             #         logger.error("memememememememememememe WRONGGGGGGGGGGGGGG")
             # else:
             #     logger.error("result is None Probably Or Size = 0")
+
     return render(request, 'problems/1.html', context)
 
 
@@ -118,8 +133,46 @@ def problem_login(request):
     return render(request, 'problems/login.html')
 
 
+def second_problem_database():
+    employees = [
+        SecondProblem(205476765, "Ariel", "Vainshtein", 25),
+        SecondProblem(306252136, "Abe", "Kelson", 22),
+        SecondProblem(268451234, "Abraham", "Yalkovitch", 28),
+    ]
+    for data in employees:
+        data.save(using="problems_db")
+
+
+'''
+    Input fields: first_name & last_name
+    To perform injection: place wildcards in both fields to get all users:
+    First Name: %
+    Last Name: %
+'''
 def second_problem(request):
-    return render(request, 'problems/2.html')
+    context = dict()
+    logger = logging.getLogger(__name__)
+    cursor1 = connections['problems_db'].cursor()
+
+    # add columns to database
+    #second_problem_database()
+
+    if request.method == 'POST':
+        with cursor1 as cursor:
+            first_name_request = request.POST.get("first_name")
+            last_name_request = request.POST.get("last_name")
+            #id_request = request.POST.get("id_request")
+            #sql = f"SELECT '{input_request}' FROM problems_secondproblem WHERE first_name LIKE 'Ariel'"
+            sql = f"SELECT id, first_name, last_name FROM problems_secondproblem WHERE first_name LIKE '{first_name_request}' and last_name LIKE '{last_name_request}'"
+            #sql = f"SELECT * FROM problems_secondproblem WHERE id={id_request}"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            logger.error("result of sql query " + result.__str__())
+            cursor.close()
+            context = {'raw_sql': sql,
+                       'result': result,
+                       'input_request': first_name_request}
+    return render(request, 'problems/2.html', context)
 
 
 def third_problem(request):
