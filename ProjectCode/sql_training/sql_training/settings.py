0194604dd4057dfcab0b6e5aae343819a0a7d6b1
Hamami <michael.hamami@s.afeka.ac.il>
2021-01-05 21:22:30 +0200

    added information about out-band and fix some design.

--- ProjectCode/sql_training/sql_training/settings.py
+++ ProjectCode/sql_training/sql_training/settings.py
@@ -98,15 +98,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 WSGI_APPLICATION = 'sql_training.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
