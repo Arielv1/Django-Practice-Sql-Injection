db4a2c25f8aa34f1993577922e338c8716563b9f
Arielv1 <arielv1994@gmail.com>
2021-02-14 17:30:52 +0200

    Removed unused code

--- ProjectCode/sql_training/sql_training/settings.py
+++ ProjectCode/sql_training/sql_training/settings.py
@@ -125,6 +125,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+#TODO - remove comments before deployment
 AUTH_PASSWORD_VALIDATORS = [
     # {
     #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
