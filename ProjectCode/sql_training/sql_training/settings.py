3ea61aa2dbae9709d4b6b61bad88455484f9a24a
Hamami <michael.hamami@s.afeka.ac.il>
2021-02-21 04:08:07 +0200

    fix mistake on settings

--- ProjectCode/sql_training/sql_training/settings.py
+++ ProjectCode/sql_training/sql_training/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'problems.apps.ProblemsConfig',
     'learn.apps.LearnConfig',
     'crispy_forms',
-    'sslserver'
+    'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
